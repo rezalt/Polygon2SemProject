35067c38f9bb4af5ebec00bc2c07fe72a15232ed
JAJAJABLESES <JAJAJABLESES@82.211.193.91>
2016-03-31 11:57:12 +0200

    user initialized

---
+++ Polygon2SemProject/src/java/User.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JAJAJABLESES
+ */
+public class User
+{
+    int id;
+    String username;
+    String password;
+
+    public
+    int getId()
+    {
+        return id;
+    }
+
+    public
+    void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public
+    String getUsername()
+    {
+        return username;
+    }
+
+    public
+    void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public
+    String getPassword()
+    {
+        return password;
+    }
+
+    public
+    void setPassword(String password)
+    {
+        this.password = password;
+    }
+    
+    
+    
+    
+    public User(int id, String username, String password)
+    {
+        
+    }
+}
