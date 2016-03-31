b01a35cb4b05c3bb3f84e1f64e257063dcffa56f
JAJAJABLESES <JAJAJABLESES@82.211.193.91>
2016-03-31 12:09:16 +0200

    user initialized

--- Polygon2SemProject/src/java/User.java
+++ Polygon2SemProject/src/java/User.java
@@ -14,38 +14,32 @@ public class User
     String username;
     String password;
 
-    public
-    int getId()
+    public int getId()
     {
         return id;
     }
 
-    public
-    void setId(int id)
+    public void setId(int id)
     {
         this.id = id;
     }
 
-    public
-    String getUsername()
+    public String getUsername()
     {
         return username;
     }
 
-    public
-    void setUsername(String username)
+    public void setUsername(String username)
     {
         this.username = username;
     }
 
-    public
-    String getPassword()
+    public String getPassword()
     {
         return password;
     }
 
-    public
-    void setPassword(String password)
+    public void setPassword(String password)
     {
         this.password = password;
     }
@@ -55,6 +49,9 @@ public class User
     
     public User(int id, String username, String password)
     {
+        this.id=id;
+        this.password=password;
+        this.username=username;
         
     }
 }
