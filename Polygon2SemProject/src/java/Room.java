90ccd7331df1b613b2ef839f8d5355418c94e71f
JAJAJABLESES <JAJAJABLESES@82.211.193.91>
2016-03-31 18:12:13 +0200

    Rooms Updated

---
+++ Polygon2SemProject/src/java/Room.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JAJAJABLESES
+ */
+public class Room
+{
+    String roomName;
+    Boolean notices;
+    Boolean damagedRoom;
+    String dateOfDamage;
+    String descriptionOfLocation;
+    String explanationOfDamage;
+    String repairs;
+    Boolean moisture;
+    Boolean sponge;
+    Boolean mold;
+    Boolean fire;
+    Boolean otherDamage;
+    String moistureDesctription;
+    String spongeDescription;
+    String moldDescription;
+    String fireDescription;
+    String otherDamageDescription;
+}
