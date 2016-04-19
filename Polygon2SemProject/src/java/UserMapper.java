08ddb883eef815a560fc7e1bce5aadae6c56cc65
kwesi1337 <kwesi1337@yahoo.dk>
2016-04-19 16:36:31 +0200

    Update UserMapper.java

--- Polygon2SemProject/src/java/UserMapper.java
+++ Polygon2SemProject/src/java/UserMapper.java
@@ -8,6 +8,32 @@
  *
  * @author josephawwal
  */
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 public class UserMapper {
+     Connection conn;
+            DBConnector DBC = new DBConnector();
+            String company;
+            
+            
+              public ArrayList<String> getUsersBuildingNames(HttpServletRequest request, HttpServletResponse response, String username)throws IOException, ServletException
+    {
+        ArrayList<String> buildingNames = new ArrayList();
+                conn = DBC.getConnection();
+
+        String sql = "SELECT * FROM building WHERE buildingCompany =?";
+        
     
+        
+        return buildingNames;
+    }
 }
